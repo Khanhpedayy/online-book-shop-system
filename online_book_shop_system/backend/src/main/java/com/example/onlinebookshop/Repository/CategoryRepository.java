f15dfb4ed3e16142a11775095b1f33e0b7157fdd
Khanhpedayy <Khanhkobeo2711@gmail.com>
2026-03-19 13:30:33 +0700

    Finalize rebase and merge

--- online_book_shop_system/backend/src/main/java/com/example/onlinebookshop/Repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.onlinebookshop.Repository;
-
-
-import com.example.onlinebookshop.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
