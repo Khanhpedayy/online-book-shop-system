b81f259396994d2118ed55e24cb900d84edeb3ee
Khanhpedayy <Khanhkobeo2711@gmail.com>
2026-03-22 17:39:35 +0700

    fix backend logic

--- online_book_shop_system/backend/src/main/java/com/example/onlinebookshop/service/BookService.java
+++ online_book_shop_system/backend/src/main/java/com/example/onlinebookshop/service/BookService.java
@@ -1,5 +1,6 @@
 package com.example.onlinebookshop.Service;
 
+import com.example.onlinebookshop.dto.BookDetailDTO;
 import com.example.onlinebookshop.dto.BookVariantDTO;
 
 import java.util.List;
@@ -9,6 +10,10 @@ public interface BookService {
     BookVariantDTO createBookVariant(BookVariantDTO dto);
 
     List<BookVariantDTO> getAllBookVariants();
+
+    List<BookVariantDTO> findBooks(String keyword, String publisherName, Double minPrice, Double maxPrice);
+
+    BookDetailDTO getBookDetail(Long variantId);
 
     BookVariantDTO getBookVariantById(Long id);
 
