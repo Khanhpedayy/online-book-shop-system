6f975aaf31c1f716fb39885d55d0a3b666f2d4bb
Khanhpedayy <Khanhkobeo2711@gmail.com>
2026-03-28 11:56:51 +0700

    final fix

---
+++ online_book_shop_system/backend/src/main/java/com/example/onlinebookshop/dto/CartItemDTO.java
@@ -0,0 +1,26 @@
+package com.example.onlinebookshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemDTO {
+    private Long id;
+    private Long variantId;
+    private Long bookId;
+    private Long copyId;
+    private Integer quantity;
+
+    private String title;
+    private String sku;
+    private BigDecimal salePrice;
+    private String coverImageUrl;
+}
+
